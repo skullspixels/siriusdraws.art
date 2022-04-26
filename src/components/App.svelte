ce66a5223d5a8061b879cfe15142c82dd7284b1e
skullspixels <skullspixels@gmail.com>
2022-04-26 10:38:47 -0300

    master

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,23 +1,16 @@
 <script>
   import { onMount } from "svelte";
-
+ 
   onMount(async () => {
-    
+    //resize screen
+    //getResize(".main");
+
   });
+
 </script>
 
-  <main>
+   <main>
     <div class="main">
-      <div class="head">
-        <ul>
-          <li class="logo">Skulls & Pixels</li>
-          <li class="description">Art creator in Tezos e ETH block chain</li>
-        </ul>
-      </div>
-      <div class="link-collection">
-        <div>
-
-        </div>
-      </div>
+	sirius draws art
     </div>
 </main>
